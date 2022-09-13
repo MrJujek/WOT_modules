f7f895f47793b67d54fe4aadce8b9c8754f256b7
MrJujek <julian.dworzycki@gmail.com>
2022-09-13 15:26:19 +0200

    js

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,9 +1,30 @@
 $(document).ready(function () {
+    $("#slots_for_equipment")
+        .show()
+
     $("#equipment")
         .css("background", "black")
         .hide()
 
-    $("svg").click(function () {
+    $("#slot1").click(function () {
+        $("#slots_for_equipment")
+            .hide()
         $("#equipment").show();
+    });
+    $("#slot2").click(function () {
+        $("#slots_for_equipment")
+            .hide()
+        $("#equipment").show();
+    });
+    $("#slot3").click(function () {
+        $("#slots_for_equipment")
+            .hide()
+        $("#equipment").show();
+    });
+
+    $("#equipment").click(function () {
+        $("#slots_for_equipment")
+            .show()
+        $("#equipment").hide();
     });
 })
