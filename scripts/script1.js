a44f00719c77f3ef6e53c7eadef1652ee9e89c7d
Julian Dworzycki <83341756+MrJujek@users.noreply.github.com>
2022-09-15 12:50:39 +0200

    test

--- scripts/script1.js
+++ scripts/script1.js
@@ -5,9 +5,9 @@ const all_tanks = 'https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?applic
 const all_equipment = 'https://api.worldoftanks.eu/wot/encyclopedia/provisions/?application_id=98e129430b80fda39a161c7a200261cf'
 
 
-//tak = tak['data']
-
-
+let wn8 = [0, 101, 201, 401, 501, 700, 1001, 1300, 1601, 1900, 2200, 2501, 3000, 3501, 4000]
+let data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
+let chart = "myChart"
 
 $(document).ready(function () {
     $("#slots_for_equipment")
@@ -21,21 +21,25 @@ $(document).ready(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
+        drawChart(wn8, data, chart)
     });
     $("#equipment_slot2").click(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
+        drawChart(wn8, data, chart)
     });
     $("#equipment_slot3").click(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
+        drawChart(wn8, data, chart)
     });
     $("#directive_slot").click(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
+        drawChart(wn8, data, chart)
     });
 
     $("#equipment").click(function () {
@@ -45,11 +49,9 @@ $(document).ready(function () {
     });
 })
 
-let wn8 = [0, 101, 201, 401, 501, 700, 1001, 1300, 1601, 1900, 2200, 2501, 3000, 3501, 4000]
-let data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-let chart = "myChart"
 
-drawChart(wn8, data, chart)
+
+
 
 let tak = (await getapi(all_tanks))["data"]
 console.log(tak)
