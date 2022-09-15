85786a4f382cfc6b42b72bb108cae2bbc4e9832e
Julian Dworzycki <83341756+MrJujek@users.noreply.github.com>
2022-09-15 12:04:09 +0200

    test

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,5 +1,5 @@
 import { getapi } from "./wg_api.mjs"
-import { drawChart } from "./chart1.js"
+import { drawChart } from "./chart1.mjs"
 
 const all_tanks = 'https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=98e129430b80fda39a161c7a200261cf&limit=10'
 const all_equipment = 'https://api.worldoftanks.eu/wot/encyclopedia/provisions/?application_id=98e129430b80fda39a161c7a200261cf'
