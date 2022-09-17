dd48c0320ad2ff591d04270d9883f37664160493
MrJujek <julian.dworzycki@gmail.com>
2022-09-17 15:30:28 +0200

    Added amuniton slot

--- scripts/charts.mjs
+++ scripts/charts.mjs
@@ -1,17 +1,17 @@
-export function drawChart(wn8_x, data_y, chart_name) {
-    const unicum = (ctx, value) => ctx.p0.parsed.y > 3500 ? value :
+export function drawWN8Chart(wn8_x, data_y, chart_name) {
+    const unicum = (ctx, value) => ((ctx.p0.parsed.y >= 3500) && (ctx.p1.parsed.y >= 3500)) ? value :
         undefined;
-    const great = (ctx, value) => ctx.p0.parsed.y > 2500 ? value :
+    const great = (ctx, value) => ((ctx.p0.parsed.y >= 2500) && (ctx.p1.parsed.y >= 2500)) ? value :
         undefined;
-    const good = (ctx, value) => ctx.p0.parsed.y > 1600 ? value :
+    const good = (ctx, value) => ((ctx.p0.parsed.y >= 1600) && (ctx.p1.parsed.y >= 1600)) ? value :
         undefined;
-    const average = (ctx, value) => ctx.p0.parsed.y > 1000 ? value :
+    const average = (ctx, value) => ((ctx.p0.parsed.y >= 1000) && (ctx.p1.parsed.y >= 1000)) ? value :
         undefined;
-    const below_average = (ctx, value) => ctx.p0.parsed.y > 500 ? value :
+    const below_average = (ctx, value) => ((ctx.p0.parsed.y >= 500) && (ctx.p1.parsed.y >= 500)) ? value :
         undefined;
-    const bad = (ctx, value) => ctx.p0.parsed.y > 200 ? value :
+    const bad = (ctx, value) => ((ctx.p0.parsed.y >= 200) && (ctx.p1.parsed.y >= 200)) ? value :
         undefined;
-    const very_bad = (ctx, value) => ctx.p0.parsed.y >= 0 ? value :
+    const very_bad = (ctx, value) => ((ctx.p0.parsed.y >= 0)) ? value :
         undefined;
 
     const wykres = new Chart(document.getElementById(chart_name), {
--- scripts/script1.js
+++ scripts/script1.js
@@ -1,13 +1,14 @@
 import { getapi } from "./wg_api.mjs"
-import { drawChart } from "./chart1.mjs"
+import { drawWN8Chart } from "./charts.mjs"
 
 const all_tanks = 'https://api.worldoftanks.eu/wot/encyclopedia/vehicles/?application_id=98e129430b80fda39a161c7a200261cf&limit=10'
 const all_equipment = 'https://api.worldoftanks.eu/wot/encyclopedia/provisions/?application_id=98e129430b80fda39a161c7a200261cf'
 
 //https://coolors.co/6f2dbd-000022-66d7d1-f7f0f5-ffbf46
-let wn8 = [0, 101, 201, 401, 501, 700, 1001, 1300, 1601, 1900, 2200, 2501, 3000, 3501, 4000]
-let data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150]
-let chart = "myChart"
+let wn8 = [0, 101, 201, 401, 501, 700, 1001, 800, 600, 200, 700, 1300, 1601, 1900, 2200, 2501, 3000, 3501, 4000]
+let data = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200]
+let chart = "WN8_chart"
+
 
 $(document).ready(function () {
     $("#slots_for_equipment")
@@ -21,25 +22,25 @@ $(document).ready(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
-        drawChart(wn8, data, chart)
+        drawWN8Chart(wn8, data, chart)
     });
     $("#equipment_slot2").click(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
-        drawChart(wn8, data, chart)
+        drawWN8Chart(wn8, data, chart)
     });
     $("#equipment_slot3").click(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
-        drawChart(wn8, data, chart)
+        drawWN8Chart(wn8, data, chart)
     });
     $("#directive_slot").click(function () {
         $("#slots_for_equipment")
             .hide()
         $("#equipment").show();
-        drawChart(wn8, data, chart)
+        drawWN8Chart(wn8, data, chart)
     });
 
     $("#equipment").click(function () {
