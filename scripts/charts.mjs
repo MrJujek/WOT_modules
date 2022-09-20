361b73defbf1262bc436f2f82c443d778f5a2380
MrJujek <julian.dworzycki@gmail.com>
2022-09-20 19:02:09 +0200

    changing names

--- scripts/charts.mjs
+++ scripts/charts.mjs
@@ -1,18 +1,11 @@
 export function drawWN8Chart(wn8, data, chart_name) {
-    const unicum = (ctx, value) => ((ctx.p0.parsed.y >= 3500) && (ctx.p1.parsed.y >= 3500)) ? value :
-        undefined;
-    const great = (ctx, value) => ((ctx.p0.parsed.y >= 2500) && (ctx.p1.parsed.y >= 2500)) ? value :
-        undefined;
-    const good = (ctx, value) => ((ctx.p0.parsed.y >= 1600) && (ctx.p1.parsed.y >= 1600)) ? value :
-        undefined;
-    const average = (ctx, value) => ((ctx.p0.parsed.y >= 1000) && (ctx.p1.parsed.y >= 1000)) ? value :
-        undefined;
-    const below_average = (ctx, value) => ((ctx.p0.parsed.y >= 500) && (ctx.p1.parsed.y >= 500)) ? value :
-        undefined;
-    const bad = (ctx, value) => ((ctx.p0.parsed.y >= 200) && (ctx.p1.parsed.y >= 200)) ? value :
-        undefined;
-    const very_bad = (ctx, value) => ((ctx.p0.parsed.y >= 0) && (ctx.p1.parsed.y >= 0)) ? value :
-        undefined;
+    const unicum = (ctx, value) => ((ctx.p0.parsed.y >= 3500) && (ctx.p1.parsed.y >= 3500)) ? value : undefined;
+    const great = (ctx, value) => ((ctx.p0.parsed.y >= 2500) && (ctx.p1.parsed.y >= 2500)) ? value : undefined;
+    const good = (ctx, value) => ((ctx.p0.parsed.y >= 1600) && (ctx.p1.parsed.y >= 1600)) ? value : undefined;
+    const average = (ctx, value) => ((ctx.p0.parsed.y >= 1000) && (ctx.p1.parsed.y >= 1000)) ? value : undefined;
+    const below_average = (ctx, value) => ((ctx.p0.parsed.y >= 500) && (ctx.p1.parsed.y >= 500)) ? value : undefined;
+    const bad = (ctx, value) => ((ctx.p0.parsed.y >= 200) && (ctx.p1.parsed.y >= 200)) ? value : undefined;
+    const very_bad = (ctx, value) => ((ctx.p0.parsed.y >= 0) && (ctx.p1.parsed.y >= 0)) ? value : undefined;
 
     const wykres = new Chart(document.getElementById(chart_name), {
         type: 'line',
